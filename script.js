3803250db798f376b4db5d61382a760bfeb085e1
cubixstudios09-rgb <cubixstudios09@gmail.com>
2026-08-02 00:19:26 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
-
 const planetas = document.querySelectorAll(".planeta");
 
 planetas.forEach((planeta) => {
     planeta.addEventListener("click", () => {
-        alert("Abrindo o portal...");
+        alert("Portal iniciado.");
     });
 });
